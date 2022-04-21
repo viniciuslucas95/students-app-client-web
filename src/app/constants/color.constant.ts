7d541869a91e44c63c029c8778fe3b6f0ad13edc
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-21 12:14:48 -0300

    Added colors variant

--- src/app/constants/color.constant.ts
+++ src/app/constants/color.constant.ts
@@ -1,4 +1,8 @@
 export const COLOR = {
     primary: '#3f51b5',
-    secondary: 'white'
+    primaryDark1: '#394aa8',
+    primaryDark2: '#2d3c91',
+    secondary: 'white',
+    secondaryDark1: '#ededed',
+    secondaryDark2: '#e2e2e2',
 }
