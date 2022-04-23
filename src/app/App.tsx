d55abdee9b984f1ace0b2febe798b1a143b71b8c
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-23 15:28:53 -0300

    Added react router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
+import { Route, Routes } from "react-router-dom";
 import { StudentsListPage } from "./pages/StudentsListPage";
 
 export function App() {
   return (
-    <StudentsListPage />
+    <Routes>
+      <Route path='/' element={<StudentsListPage />} />
+    </Routes>
   )
 }
