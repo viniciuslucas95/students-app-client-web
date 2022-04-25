06b67240a74549cfa5f15a26157ed3e5006ec838
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-25 15:37:14 -0300

    Fixed svgs

--- src/assets/svgs/Svg.tsx
+++ src/assets/svgs/Svg.tsx
@@ -8,7 +8,7 @@ interface IProps {
     rotation?: number
 }
 
-export function Svg({ children, rotation = 0, size = 'big' }: PropsWithChildren<IProps>) {
+export function Svg({ children, rotation = 0, size = 'small' }: PropsWithChildren<IProps>) {
     const sizeValue = size === 'big' ? 48 : size === 'medium' ? 36 : 24
 
     return <svg style={{ transform: `rotate(${rotation}deg)` }} width={sizeValue} height={sizeValue} viewBox='0 0 48 48' fill="none" xmlns="http://www.w3.org/2000/svg" >
